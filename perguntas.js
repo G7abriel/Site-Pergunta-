eb122180d66922fcd63234ec7559d9ab95b648b7
G7abriel <141252861+G7abriel@users.noreply.github.com>
2024-10-23 09:27:05 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'História',
+    'Quando iniciou a Primeira Guerra Mundial?',
+    'Iniciou em 1914'
+)
+
+criaCartao(
+    'Historia',
+    'Em que ano caiu o muro de Berlim, marcando o fim da Guerra fria?',
+    'Caiu em 1989'
+)
+
+criaCartao(
+    'Historia',
+    'Quem foi o primeiro Inperador do Basil?',
+    'Dom Pedro I'
+)
+
+criaCartao(
+    'História',
+    'Qual evento marcou o inicio da Segunda Guerra Mundial?',
+    'A invasão da Polonia pela Alemanha'
+)
